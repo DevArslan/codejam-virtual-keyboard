0517a78b3ff2fd67db257b1e4abe525eab999155
DevArslan <arslan.gareev.2017@gmail.com>
2019-11-06 23:30:33 +0300

    add if gh-pages

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ textarea.className = 'textareaResult' ;
 
 
 const keys = ['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','/','del','CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','enter','Shift','я','ч','с','м','и','т','ь','б','ю','.','▲','Shift','Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl',]
+const keys_en = ['`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab','q','w','e','r','t','y','u','i','o','p','[',']','|','del','CapsLock','a','s','d','f','g','h','j','k','l',';','"','enter','Shift','z','x','c','v','b','n','m',',','<','>','▲','Shift','Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl',]
+
+const keyCode = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace", "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight","Backslash", "Delete", "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter", "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma","Period","Slash","ArrowUp", "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ArrowLeft", "ArrowDown", "ArrowRight", "ControlRight"]
+
+
+let saveKeys = []
+
+
 
 function createKeyboard() {
 
@@ -80,28 +88,97 @@ function createKeyboard() {
 
 
 }
-function insertValueKey(keys) {
+function insertValueKey(keys,keyCode,keys_en) {
     
     let keyList = document.getElementsByClassName('key')
     
-    for (key in keyList){
+    for (let key = 0; key < 64; key++){
         let keyValue = document.createElement('span')
+        keyValue.className = localStorage.getItem('first')
         keyValue.innerHTML = keys[key];
         
         keyList[key].appendChild(keyValue)
 
     }
+    for (let key = 0; key < 64; key++){
+        let keyValue = document.createElement('span')
+        keyValue.innerHTML = keys_en[key];
+        keyValue.className = localStorage.getItem('last')
+        keyList[key].appendChild(keyValue)
+
+    }
+    for (let key = 0; key < 64; key++){
+
+        keyList[key].classList.add(keyCode[key])
+
+
+    }
 
 }
 
+function changeLanguage() {
+    document.addEventListener('keydown', function(event) {
+        if (saveKeys[0] != event.code){
+            if (event.code == 'ShiftLeft' || event.code == 'AltLeft'){
+                saveKeys.push(event.code)
+            }
+        }
+        if (saveKeys[0] == 'ShiftLeft' && saveKeys[1] == 'AltLeft'){
+            let keyList = document.getElementsByClassName('key')
+    
+            for (let key = 0; key < 64; key++){
+                
+                if (keyList[key].firstElementChild.className == 'on'){
+                    localStorage.setItem('first', 'off')
+                    localStorage.setItem('last', 'on')
+                    keyList[key].firstElementChild.className = localStorage.getItem('first')
+                    keyList[key].lastElementChild.className = localStorage.getItem('last')
+                    console.log(localStorage)
+                }
+                else{
+                    localStorage.setItem('first', 'on')
+                    localStorage.setItem('last', 'off')
+                    keyList[key].firstElementChild.className = localStorage.getItem('first')
+                    keyList[key].lastElementChild.className = localStorage.getItem('last')
+                    console.log(localStorage)
+                }
+                
 
+            }
+        }
+    });
+}
+listKeys = []
+function keyDown() {
+    document.addEventListener('keydown', function(event) {
+        
+        
+        let selectKey = document.getElementsByClassName(event.code)
+        
+        selectKey[0].classList.add('down')
+
+      });
+}
+
+function keyUp() {
+    document.addEventListener('keyup', function(event) {
+        
+        
+        let selectKey = document.getElementsByClassName(event.code)
+        
+        selectKey[0].classList.remove('down')
+        saveKeys = []
+
+
+      });
+}
 
 
 createKeyboard()
-insertValueKey(keys)
-
-
-
+insertValueKey(keys,keyCode,keys_en)
+keyDown()
+keyUp()
+changeLanguage()
 
 
 
